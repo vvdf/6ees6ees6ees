dd5e6542e792aa28e67f473b69cef249bfc842c6
7FFF00 <t42please@gmail.com>
2019-12-16 10:59:03 -0800

    Implement RetiredForagerBee class that extends ForagerBee using es6

--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,3 +1,17 @@
-class RetiredForagerBee {
-  // TODO..
+class RetiredForagerBee extends ForagerBee {
+  constructor() {
+    super();
+    this.age = 40;
+    this.job = "gamble";
+    this.color = "grey";
+    this.canFly = false;
+  };
+
+  forage() {
+    return "I am too old, let me play cards instead";
+  };
+
+  gamble() {
+    this.treasureChest.push("treasure");
+  };
 };
